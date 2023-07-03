92ee55141ef487547707dfc11b8c810b0f59c58d
Alexander-run <advance@AdvancedeMacBook-Pro.local>
2023-07-03 21:13:04 +0800

    u

--- fnovatech/src/pages/Contact.vue
+++ fnovatech/src/pages/Contact.vue
@@ -1,11 +1,105 @@
 <template>
-  <div>
-    Contact page
-  </div>
+  <div class='contact'>
+    <h1>Drop us a line!</h1>
+    <el-form 
+      ref="formRef"
+      class='form'
+      :model="form"
+      label-width="80px"
+      :rules="rules"
+    >
+      <el-form-item label="Name" prop="name">
+        <el-input 
+          class='singleInput'
+          type='text'
+          placeholder='Name'
+          v-model="form.name"
+        />
+      </el-form-item>
+      <el-form-item label="Mobile" prop="mobile">
+        <el-input 
+          class='singleInput'
+          type='text'
+          placeholder='Mobile'
+          v-model="form.mobile"
+        />
+      </el-form-item>
+      <el-form-item label="Email" prop="email">
+        <el-input 
+          class='singleInput'
+          type='email'
+          placeholder='Email'
+          v-model="form.email"
+        />
+      </el-form-item>
+      <el-form-item label="Address" prop="address">
+        <el-input 
+          class='singleInput'
+          type='text'
+          placeholder='Address'
+          v-model="form.address"
+        />
+      </el-form-item>
+      <el-form-item label="Message" prop="message">
+        <el-input 
+          class='richInput'
+          type='textarea'
+          :rows="4"
+          placeholder='Message'
+          v-model="form.message"
+        />
+      </el-form-item>
+      <el-form-item>
+        <el-button 
+          class='submitButton'
+          type="primary"
+          @click="onHandleClick(formRef)"
+        >
+          Send a Message
+        </el-button>
+      </el-form-item>
+    </el-form>
+  </div> 
 </template>
 
-<script>
-export default {
-  name: 'Contact'
+<script setup>
+import { ref } from 'vue'
+import { ElMessage } from 'element-plus'
+
+const rules = {
+  name: [
+    { required: true, message: 'Please tell us how to call you', trigger: 'blur' },
+  ],
+  email: [
+    { required: true, message: 'Please input email address', trigger: 'blur' },
+  ],
+  message: [
+    { required: true, message: 'Please tell us what you want to say', trigger: 'blur' },
+  ],
+}
+const formRef = ref()
+const form = ref({
+  name: '',
+  mobile: '',
+  email: '',
+  address: '',
+  message: ''
+})
+
+const onHandleClick = async (formEl) => {
+  if (!formEl) return 
+  await formEl.validate((valid) => {
+    if (valid) {
+      ElMessage({
+        type: 'success',
+        message: 'Message sent successfully!'
+      })
+    } else return
+  })
 }
 </script>
+<style lang="less" scoped>
+.form {
+  min-width: 500px;
+}
+</style>
--- fnovatech/src/pages/Residential.vue
+++ fnovatech/src/pages/Residential.vue
@@ -1,20 +1,18 @@
 <template>
   <div class='residential'>
-    <el-row>
-      <h1>Tell us your seasonal Electric Charge</h1>
-    </el-row>      
-    <el-row>
+    <h1>Tell us your seasonal Electric Charge</h1>
+    <el-row class="row">
       <el-input-number v-model="elCharge" :min="0" :max="1200" label="upper than $1200 may be applied for commercial plan" :precision="2" :step="1" controls-position="right"/>
-      <el-button @click="onDisplayPlan">See your plan</el-button>
+      <el-button class="btn" @click="onDisplayPlan">See your plan</el-button>
     </el-row>         
-    <el-row v-if="displayOpts && displayOpts.length>0">
+    <el-row v-if="displayOpts && displayOpts.length>0" class="row">
       <div v-for="(item, index) in displayOpts" :key="index" class='residential_opt'>
         <h3>{{item.panelCount * item.unitPower / 1000}} kW 
         ({{item.panelCount}} x {{item.unitPower}} W)</h3>
         <p>with {{item.inverterPower}} kW inverter</p>
         <p>All panels with your chosen!</p>
-        <el-button @click="direct('/contact')">
-            <Button type='primary'>Start your solar life now!</Button>
+        <el-button class="btn" @click="direct('/contact')">
+          Start your solar life now!
         </el-button>
       </div>
     </el-row>
@@ -23,6 +21,8 @@
 
 <script setup>
 import { ref } from 'vue'
+import { useRouter } from 'vue-router'
+const router = useRouter()
 
 const residentialOpts = [
   {panelCount:18,unitPower:370,inverterPower:5},
@@ -47,8 +47,32 @@ const onDisplayPlan = () => {
   }
   displayOpts.value = residentialOpts.slice(2*index,2*index+2)
 }
-const direct = (key) => () => {
+const direct = (key) => {
   router.push(key)
 }
 
 </script>
+<style lang="less" scoped>
+.residential {
+  margin: 20px;
+}
+.row {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  .btn {
+    margin-left: 12px;
+    background: lightblue;
+    border: lightblue;
+    color: white;
+  }
+  .residential_opt {
+    border: 1px solid lightblue;
+    box-shadow: 0 0 8px lightblue;
+    border-radius: 8px;
+    margin: 8px;
+    padding: 8px;
+  }
+}
+</style>
